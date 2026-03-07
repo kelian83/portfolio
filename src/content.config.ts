1ecc3dfe9f7f8747e374762b60f2f15582d62372
Kelian-CAZENEUVE <kelian.cazeneuve@etu.unilim.fr>
2026-03-07 16:37:00 +0100

    portfolio_4.0.1

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,6 @@
 import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-		tags: z.array(z.string()).optional(),
-	}),
-});
-
 const projects = defineCollection({
 	loader: glob({ base: './src/content/projects', pattern: '**/*.{md,mdx}' }),
 	schema: z.object({
@@ -25,4 +13,4 @@ const projects = defineCollection({
 	}),
 });
 
-export const collections = { blog, projects };
+export const collections = { projects };
